8015b13249a53d02c0f0935a5212d9d8fe43a9ae
H <93381523+Rgoogle@users.noreply.github.com>
2022-09-21 00:05:42 +0800

    Update Auto.sh

--- Auto.sh
+++ Auto.sh
@@ -1,4 +1,14 @@
 apt update
 apt install nohup
-nohup ((echo y && echo 111111 && echo 111111 && echo 13742) |(bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh))
-((echo 2 && echo 3 && echo 3 && echo 1 &) | (wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh))) &
+
+touch a
+touch b
+chmod 777 a
+chmod 777 b
+(echo y && echo 111111 && echo 111111 && echo 13742) > a
+(echo 2 && echo 3 && echo 3 && echo 1 ) > b
+
+sudo nohup wget -N https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh && bash install.sh <./a &
+sudo nohup wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh < ./b &
+
+rm a b
