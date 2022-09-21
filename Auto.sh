22bf5a80871efbb7ef738c47d4d6a6170e8f835f
H <93381523+Rgoogle@users.noreply.github.com>
2022-09-21 08:49:14 +0800

    Update Auto.sh

--- Auto.sh
+++ Auto.sh
@@ -7,7 +7,6 @@ chmod 777 b
 (echo y && echo 111111 && echo 111111 && echo 13742) > a
 (echo 2 && echo 3 && echo 3 && echo 1 ) > b
 
-sudo nohup wget -N https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh && bash install.sh <./a &
-sudo nohup wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh < ./b &
+sudo wget -N https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh && bash install.sh <./a && sudo nohup wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh < ./b &
 
 
